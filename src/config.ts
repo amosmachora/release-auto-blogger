2748158a00d9c75b3a847ada34a752bfd9e4f4f7
Amoh Prince <81857018+AmohPrince@users.noreply.github.com>
2024-02-01 15:22:12 +0300

    logging out user workspace

--- src/config.ts
+++ src/config.ts
@@ -2,3 +2,6 @@ export const projectName = process.env.PROJECT_NAME;
 export const hashnodeHost = process.env.HASHNODE_HOST;
 export const subtitle = process.env.SUBTITLE;
 export const coverImageURL = process.env.COVER_IMAGE;
+export const userWorkspace = process.env.GITHUB_WORKSPACE;
+
+console.log(userWorkspace);
