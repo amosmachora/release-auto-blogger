9a768f5f3269de88dd8d81ec5c03ead0f6be5789
Amoh Prince <81857018+AmohPrince@users.noreply.github.com>
2024-02-01 14:21:03 +0300

    making action rely on itself

--- dist/config.d.ts
+++
@@ -1,6 +0,0 @@
-declare const projectName: any;
-declare const hashnodeHost: any;
-declare const subtitle: any;
-declare const coverImageURL: any;
-
-export { coverImageURL, hashnodeHost, projectName, subtitle };
--- dist/publications.d.ts
+++
@@ -1,3 +0,0 @@
-declare const getPublicationId: () => Promise<any>;
-
-export { getPublicationId };
