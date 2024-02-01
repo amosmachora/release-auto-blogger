c7359f1d25d1ab2902fae849ed573d4a8c0cd305
Amoh Prince <81857018+AmohPrince@users.noreply.github.com>
2024-02-01 15:09:43 +0300

    configured configs

--- src/config.ts
+++ src/config.ts
@@ -1,13 +1,4 @@
-const core = require("@actions/core");
-
-export const projectName = core.getInput("project-name");
-export const hashnodeHost = core.getInput("hashnode-host");
-export const subtitle = core.getInput("subtitle");
-export const coverImageURL = core.getInput("cover-image");
-
-// export const projectName = "New Name";
-// export const hashnodeHost = "amosmachora.hashnode.dev";
-// export const subtitle = "sub";
-// export const coverImageURL = "some uri";
-
-console.log(process.env.PROJECT_NAME, "project-name");
+export const projectName = process.env.PROJECT_NAME;
+export const hashnodeHost = process.env.HASHNODE_HOST;
+export const subtitle = process.env.SUBTITLE;
+export const coverImageURL = process.env.COVER_IMAGE;
